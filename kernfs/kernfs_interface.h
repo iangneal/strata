505016997ef9d724d056e3be71fceb868bff9cd7
Ian Glen Neal <ian.gl.neal@gmail.com>
2018-02-05 01:44:09 -0600

    Fixed up extent_tests.cc to compile.
    Added a C version of libcuckoo.

--- kernfs/kernfs_interface.h
+++ kernfs/kernfs_interface.h
@@ -1,7 +1,16 @@
 #ifndef _KERN_FS_INTERFACE_
 #define _KERN_FS_INTERFACE_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void init_fs(void);
 
 void shutdown_fs(void);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
